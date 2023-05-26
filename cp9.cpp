13919dd5462b067f2dacb1c8970eb5aefe1efebc
chandu <cmhandan@gmail.com>
2023-05-26 20:36:55 +0530

    added #cp9 of telegram group

--- cp9.cpp
+++ cp9.cpp
@@ -1,8 +1,4 @@
-// THis program is based on a problem in codechef
-//! THis is based on Keplers law
-/*Kepler’s Law states that the planets move around the sun in elliptical orbits with the sun at one focus. Kepler's 3rd law is The Law of Periods, according to which:
-
-The square of the time period of the planet is directly proportional to the cube of the semimajor axis of its orbit.*/
+// ! this program is the simple one where i have seperated every digit and find the number 4 
 #include <iostream>
 using namespace std;
 
@@ -12,13 +8,16 @@ int main() {
 	cin >> t;
 	while(t--)
 	{
-	    float a,b,c,d;
-	    cin >> a>>b>>c>>d;
-	    float res1,res2;
-	    res1 = (a*a)/(c*c*c);
-	    res2 = (b*b)/(d*d*d);
-	    if(res1==res2) cout << "Yes" << endl;
-	    else cout << "No" << endl;
+	    int num;
+	    cin >> num;
+	    int c = 0,res=0;
+	    while(num!=0)
+	    {
+	        res = num%10;
+	        if(res==4) c++;
+	        num = num/10;
+	    }
+	    printf("%d\n",c);
 	}
 	return 0;
 }
