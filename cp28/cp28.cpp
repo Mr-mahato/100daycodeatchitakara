34851545a65dcad85827b8d04e80fe73f3957b93
chandu <cmhandan@gmail.com>
2023-06-20 13:22:05 +0530

    This is the problem fit square in triangle

---
+++ cp28/cp28.cpp
@@ -0,0 +1,18 @@
+ // problem link :https://www.codechef.com/problems/TRISQ?tab=statement
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int x;
+	    cin >> x;
+	    cout << ((x/2 -1) *(x/2))/2 << endl;
+	    
+	}
+	return 0;
+}
