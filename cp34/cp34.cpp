55160d2813836403716e9ffb78ce80d394d0230b
chandu <cmhandan@gmail.com>
2023-06-20 13:24:46 +0530

    Its the problem of rating dilemma of COdechef

---
+++ cp34/cp34.cpp
@@ -0,0 +1,19 @@
+// problem link :https://www.codechef.com/problems/RATING?tab=statement
+
+// I was amazed looking the solution of this problem 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int s;
+	    cin >> s;
+	    cout << 0 - (s+1)<< endl;
+	}
+	return 0;
+}
