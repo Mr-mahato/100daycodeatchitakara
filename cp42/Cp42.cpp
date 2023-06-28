a40060d4883faf0cd1ba72ddc140fc5abd8b0f86
chandan mahato <cmhandan@gmail.com>
2023-06-28 18:48:40 +0530

    added cp42

---
+++ cp42/Cp42.cpp
@@ -0,0 +1,17 @@
+// problem link :https://www.codechef.com/problems/COMPCAND
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n ,k;
+	    cin >> n >> k;
+	    if(n%k==0) cout << n/k << endl;
+	    else cout << -1 << endl;
+	}
+	return 0;
+}
