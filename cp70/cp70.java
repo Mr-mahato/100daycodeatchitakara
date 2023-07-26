0f8df59f57eb29572a3d0c853605696f109d998c
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-07-26 22:04:24 +0530

    this problem find the duplicate around it

---
+++ cp70/cp70.java
@@ -0,0 +1,28 @@
+public class cp70 {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            int c = 0;
+            for(int i=0;i<n-1;i++)
+            {
+                if(arr[i]==arr[i+1])
+                {
+                    c++;
+                }
+            }
+            System.out.println(n-c);
+
+        }
+    }
+}
