4e55cc34d7c5040009b35c634963dfe8ccc6ac00
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-07-27 22:57:40 +0530

    pushpa raj in chefland

---
+++ cp71/cp71.java
@@ -0,0 +1,35 @@
+public class cp71 {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[]arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            Arrays.sort(arr);
+            if(n==1){
+                System.out.println(arr[0]);
+            }
+            else{
+                long count=0;
+                long sum=0;
+                for(int i=n-1;i>=1;i--){
+                    if(arr[i]==arr[i-1]){
+                        count++;
+                    }
+                    else{
+                        if(sum<arr[i]+count){
+                            sum=arr[i]+count;
+                        }
+                        count=0;
+                    }
+                }
+                System.out.println(sum);
+            }
+        }
+    }
+}
