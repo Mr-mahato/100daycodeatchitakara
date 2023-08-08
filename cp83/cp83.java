5b1657b0678b5c111e753da0de9e47d217eee1f0
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-08-08 20:39:30 +0530

    This will find the time for chef to be rich

---
+++ cp83/cp83.java
@@ -0,0 +1,17 @@
+// problem link :https://www.codechef.com/problems/CHFRICH
+public class cp83 {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int a = sc.nextInt() , b = sc.nextInt() , x = sc.nextInt();
+            a = b-a;
+            System.out.println(a/x);
+
+        }
+    }
+
+}
