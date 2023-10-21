d0c5c4b0c299e9a3013c391dfe1a9017aabc9899
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-10-21 09:32:39 +0545

    this is the camelCase problem in java

--- cp95/CamelCase.java
+++ cp95/CamelCase.java
@@ -1,24 +1,24 @@
-public class CamelCase {
-    import java.lang.StringBuilder;
-    class Solution{
+class CamelCase {
+//    import java.lang.StringBuilder;
+   class Solution{
 
-        static String toCamelCase(String s){
-            StringBuilder temp = new StringBuilder(s);
-            for(int i =0;i<temp.length();i++)
-            {
-                int storeInd=0;
-                if(temp.charAt(i)=='-' || temp.charAt(i)=='_')
-                {
-                    storeInd = i;
-                    temp.deleteCharAt(i);
-                }
-                if(storeInd !=0){
-                    char ch = temp.charAt(storeInd);
-                    temp.setCharAt(i,Character.toUpperCase(ch));
-                }
-            }
+       static String toCamelCase(String s){
+           StringBuilder temp = new StringBuilder(s);
+           for(int i =0;i<temp.length();i++)
+           {
+               int storeInd=0;
+               if(temp.charAt(i)=='-' || temp.charAt(i)=='_')
+               {
+                   storeInd = i;
+                   temp.deleteCharAt(i);
+               }
+               if(storeInd !=0){
+                   char ch = temp.charAt(storeInd);
+                   temp.setCharAt(i,Character.toUpperCase(ch));
+               }
+           }
 
-            return temp.toString();
-        }
-    }
+           return temp.toString();
+       }
+   }
 }
